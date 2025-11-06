b138ed8b9c8739e41b94f3f0da2564b98d13a61b
farhannajedi <najedifarhan@gmail.com>
2025-11-06 10:18:37 +0700

    add opd configs migration

---
+++ database/migrations/2025_11_06_031717_create_opd_configs_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('opd_configs', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('opd_id')->constrained()->cascadeOnDelete();
+            $table->string('logo')->nullable();
+            $table->string('favicon')->nullable();
+            $table->string('address')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('email')->nullable();
+            $table->string('facebook_url')->nullable();
+            $table->string('instagram_url')->nullable();
+            $table->string('twitter_url')->nullable();
+            $table->string('tiktok_url')->nullable();
+            $table->string('youtube_url')->nullable();
+            $table->string('homepage_layout')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('opd_configs');
+    }
+};
