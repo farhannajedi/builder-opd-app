e3556476c5978096781974b0a6ff00ae3dde9320
farhannajedi <najedifarhan@gmail.com>
2025-11-07 08:04:58 +0700

    add data migration

---
+++ database/migrations/2025_11_07_010323_create_planning_documents_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('planning_documents', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('opd_id')->constrained('opd')->cascadeOnDelete();
+            $table->string('title');
+            $table->longText('content');
+            $table->string('file');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('planning_documents');
+    }
+};
