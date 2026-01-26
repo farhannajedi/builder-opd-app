2564f4a3f1e8d6173dbc5b19d159da8048674293
farhannajedi <najedifarhan@gmail.com>
2026-01-26 07:55:05 +0700

    change style planning dokumen

--- resources/views/components/sections/planning-dokumen.blade.php
+++ resources/views/components/sections/planning-dokumen.blade.php
@@ -7,34 +7,37 @@ $opdName = $latestDocuments->first()?->opd?->name ?? 'Instansi';
 @endphp
 
 <div class="max-w-screen-lg mx-auto w-full">
-    <section class="max-w-screen-xl px-4 mx-auto w-full py-2 md:py-4">
+    <section class="max-w-screen-xl px-2 mx-auto w-full py-2 md:py-2">
         <!-- header halaman -->
-        <div class="pt-4 mb-2 border-gray-300 pb-3">
-            <p class="text-2xl md:text-2xl font-medium text-slate-700">Dokumen Perencanaan {{$opdName}}</p>
+        <div class="flex justify-center pt-4 mb-2 border-gray-300 pb-3">
+            <h1 class="text-3xl md:text-2xl font-medium text-slate-700">Dokumen Perencanaan {{$opdName}}</h1>
         </div>
 
         <!-- konten container utama -->
         <div class="grid grid-cols-1 gap-10">
             <!-- list dokuemen -->
             <div class="bg-white p-6 md:p-8 rounded-xl shadow-lg border border-gray-200">
-                <p class="text-2xl font-semibold text-gray-700 mb-6 border-b pb-4">
+                <p class="flex justify-center text-xl font-semibold text-gray-700 mb-2 pb-2">
                     <!-- border-b memberikan garis dibawah teks -->
                     Arsip Dokumen Perencanaan
+                <div
+                    class="w-full md:w-l h-0.5 mx-auto bg-gradient-to-r from-transparent via-orange-500 to-transparent">
+                </div>
                 </p>
 
-                <div class="space-y-4">
+                <div class="space-y-4 mt-4">
                     @forelse($latestDocuments as $doc)
 
                     <!-- link to detail halaman -->
                     <a href="/planning-dokumen/{{ $doc->slug }}"
-                        class="block border border-gray-200 rounded-lg p-4 transition duration-300 hover:border-blue-400 bg-white group"
+                        class="block border border-gray-200 rounded-lg p-4 transition duration-300 hover:border-orange-400 bg-white group"
                         style="text-decoration: none;">
                         <div class="flex item-start justify-between gap-4">
                             <!-- detail dokumen -->
                             <div class="flex items-start gap-4 flex-grow">
                                 <!-- icon file statis -->
                                 <div class="bg-blue-100 rounded-lg p-3 flex-none">
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 text-blue-600"
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 text-orange-600"
                                         viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
                                         stroke-linecap="round" stroke-linejoin="round">
                                         <path d="M14 3v4a1 1 0 0 0 1 1h4" />
@@ -45,7 +48,7 @@ $opdName = $latestDocuments->first()?->opd?->name ?? 'Instansi';
                                 <!-- detail dokumen -->
                                 <div class="flex-grow space-y-1">
                                     <p
-                                        class="text-lg font-bold text-gray-800 line-clamp-2 group-hover:text-blue-600 transition duration-150">
+                                        class="text-lg font-bold text-gray-800 line-clamp-2 group-hover:text-orange-600 transition duration-150">
                                         {{ $doc->title }}
                                     </p>
 
@@ -78,7 +81,7 @@ $opdName = $latestDocuments->first()?->opd?->name ?? 'Instansi';
                             <!-- status dan indikator link -->
                             <div class="flex-none flex items-center gap-2">
                                 <div
-                                    class="bg-blue-600 text-white rounded-lg px-3 py-2 text-sm font-medium flexitems-center gap-1">
+                                    class="bg-orange-600 text-white rounded-lg px-3 py-2 text-sm font-medium flex items-center gap-1">
                                     <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 24 24"
                                         fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
                                         stroke-linejoin="round">
@@ -86,20 +89,16 @@ $opdName = $latestDocuments->first()?->opd?->name ?? 'Instansi';
                                         <path d="M17 21H7a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h7l5 5v11a2 2 0 0 1-2 2z" />
                                     </svg>
                                     Detail File
-                                </div>
-
-                                <!-- tombol panah link -->
-                                <div class="hidden md:block">
-                                    <svg xmlns="http://www.w3.org/2000/svg"
-                                        class="h-6 w-6 text-blue-500 group-hover:translate-x-1 transition duration-150"
-                                        viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                                        stroke-linecap="round" stroke-linejoin="round">
-                                        <path d="M5 12h14M12 5l7 7-7 7" />
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                        stroke-width="1.5" stroke="currentColor" class="size-4">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                            d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25"></path>
                                     </svg>
                                 </div>
                             </div>
                         </div>
                     </a>
+
                     <!-- jika menggunakan forelse harus ada logika apabila data kosong -->
                     @empty
                     <div class="bg-gray-100 p-8 text-center rounded-lg text-gray-600">
@@ -107,6 +106,19 @@ $opdName = $latestDocuments->first()?->opd?->name ?? 'Instansi';
                     </div>
                     @endforelse
                 </div>
+                <footer class="flex pt-4 items-center gap-4">
+                    <div class="flex-grow border-b border-yellow-500"></div>
+                    <a wire:navigate="" href="https://merantikab.go.id/publikasi-dokumen"
+                        class="inline-flex items-center gap-2 border border-slate-200 px-4 py-2 rounded-xl bg-orange-500 hover:bg-orange-600 text-white transition">
+                        Selengkapnya
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                            stroke="currentColor" class="size-4">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25">
+                            </path>
+                        </svg>
+                    </a>
+                </footer>
             </div>
         </div>
     </section>
