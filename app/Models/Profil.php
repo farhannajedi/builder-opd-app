08db4b99a9e7bb89ea5eef0e78d196484a493fed
farhannajedi <najedifarhan@gmail.com>
2026-03-18 12:28:17 +0700

    add data profil instansi

--- app/Models/Profil.php
+++ app/Models/Profil.php
@@ -2,9 +2,46 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 
 class Profil extends Model
 {
-    //
+    protected $fillable = [
+        'opd_id',
+        'nama_kepala_dinas',
+        'sambutan_kepala',
+        'gambar',
+        'tentang_kami',
+        'visi',
+        'misi',
+        'penjelasan_tugas',
+        'tugas',
+        'fungsi',
+        'bagan_struktur',
+    ];
+
+    protected $casts = [
+        'published_at' => 'datetime',
+    ];
+
+    // isi slug otomatis
+    protected static function booted()
+    {
+        $slug = getenv('APP_ID');
+
+        if ($slug) {
+            static::addGlobalScope('filterOPD', function (Builder $builder) use ($slug) {
+                // Mencari berita yang memiliki relasi ke tabel OPD dengan slug tertentu
+                $builder->whereHas('opd', function ($query) use ($slug) {
+                    $query->where('slug', $slug);
+                });
+            });
+        }
+    }
+
+    public function opd()
+    {
+        return $this->belongsTo(Opd::class, 'opd_id');
+    }
 }
--- database/migrations/2026_03_18_043759_create_profil_table.php
+++ database/migrations/2026_03_18_043759_create_profil_table.php
@@ -13,6 +13,18 @@ return new class extends Migration
     {
         Schema::create('profil', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('opd_id')->constrained()->onDelete('cascade');
+            $table->string('nama_kepala_dinas')->nullable();
+            $table->text('sambutan_kepala')->nullable();
+            $table->string('gambar')->nullable();
+            $table->text('tentang_kami')->nullable();
+            $table->text('visi')->nullable();
+            $table->text('misi')->nullable();
+            $table->string('penjelasan_tugas')->nullable();
+            $table->string('tugas')->nullable();
+            $table->string('fungsi')->nullable();
+            $table->string('bagan_struktur')->nullable();
+            $table->date('published_at')->nullable();
             $table->timestamps();
         });
     }
